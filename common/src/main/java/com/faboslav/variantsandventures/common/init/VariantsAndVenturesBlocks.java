323a056e2f3fa68b4fa8f9daeefa12e905743530
Aleš Fabiánek <fabianek.ales@gmail.com>
2024-03-02 18:17:10 +0100

    1.20.1

--- common/src/main/java/com/faboslav/variantsandventures/common/init/VariantsAndVenturesBlocks.java
+++ common/src/main/java/com/faboslav/variantsandventures/common/init/VariantsAndVenturesBlocks.java
@@ -10,7 +10,8 @@ import com.faboslav.variantsandventures.common.mixin.WallSkullBlockAccessor;
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
-import net.minecraft.block.Material;
+import net.minecraft.block.enums.Instrument;
+import net.minecraft.block.piston.PistonBehavior;
 import net.minecraft.registry.Registries;
 
 import java.util.function.Supplier;
@@ -22,12 +23,12 @@ public final class VariantsAndVenturesBlocks
 {
 	public static final ResourcefulRegistry<Block> BLOCKS = ResourcefulRegistries.create(Registries.BLOCK, VariantsAndVentures.MOD_ID);
 
-	public static RegistryEntry<Block> GELID_HEAD = BLOCKS.register("gelid_head", () -> SkullBlockAccessor.variantsandventures$createSkullBlock(SkullBlockType.GELID.get(), AbstractBlock.Settings.of(Material.DECORATION).strength(1.0F)));
-	public static Supplier<Block> GELID_WALL_HEAD = BLOCKS.register("gelid_wall_head", () -> WallSkullBlockAccessor.variantsandventures$createWallSkullBlock(SkullBlockType.GELID.get(), AbstractBlock.Settings.of(Material.DECORATION).strength(1.0F).dropsLike(GELID_HEAD.get())));
-	public static Supplier<Block> THICKET_HEAD = BLOCKS.register("thicket_head", () -> SkullBlockAccessor.variantsandventures$createSkullBlock(SkullBlockType.THICKET.get(), AbstractBlock.Settings.of(Material.DECORATION).strength(1.0F)));
-	public static Supplier<Block> THICKET_WALL_HEAD = BLOCKS.register("thicket_wall_head", () -> WallSkullBlockAccessor.variantsandventures$createWallSkullBlock(SkullBlockType.THICKET.get(), AbstractBlock.Settings.of(Material.DECORATION).strength(1.0F).dropsLike(THICKET_HEAD.get())));
-	public static Supplier<Block> VERDANT_SKULL = BLOCKS.register("verdant_skull", () -> SkullBlockAccessor.variantsandventures$createSkullBlock(SkullBlockType.VERDANT.get(), AbstractBlock.Settings.of(Material.DECORATION).strength(1.0F)));
-	public static Supplier<Block> VERDANT_WALL_SKULL = BLOCKS.register("verdant_wall_skull", () -> WallSkullBlockAccessor.variantsandventures$createWallSkullBlock(SkullBlockType.VERDANT.get(), AbstractBlock.Settings.of(Material.DECORATION).strength(1.0F).dropsLike(VERDANT_SKULL.get())));
+	public static RegistryEntry<Block> GELID_HEAD = BLOCKS.register("gelid_head", () -> SkullBlockAccessor.variantsandventures$createSkullBlock(SkullBlockType.GELID.get(), AbstractBlock.Settings.create().instrument(Instrument.ZOMBIE).strength(1.0F).pistonBehavior(PistonBehavior.DESTROY)));
+	public static Supplier<Block> GELID_WALL_HEAD = BLOCKS.register("gelid_wall_head", () -> WallSkullBlockAccessor.variantsandventures$createWallSkullBlock(SkullBlockType.GELID.get(), AbstractBlock.Settings.create().instrument(Instrument.ZOMBIE).strength(1.0F).pistonBehavior(PistonBehavior.DESTROY).dropsLike(GELID_HEAD.get())));
+	public static Supplier<Block> THICKET_HEAD = BLOCKS.register("thicket_head", () -> SkullBlockAccessor.variantsandventures$createSkullBlock(SkullBlockType.THICKET.get(),AbstractBlock.Settings.create().instrument(Instrument.ZOMBIE).strength(1.0F).pistonBehavior(PistonBehavior.DESTROY)));
+	public static Supplier<Block> THICKET_WALL_HEAD = BLOCKS.register("thicket_wall_head", () -> WallSkullBlockAccessor.variantsandventures$createWallSkullBlock(SkullBlockType.THICKET.get(), AbstractBlock.Settings.create().instrument(Instrument.ZOMBIE).strength(1.0F).pistonBehavior(PistonBehavior.DESTROY).dropsLike(THICKET_HEAD.get())));
+	public static Supplier<Block> VERDANT_SKULL = BLOCKS.register("verdant_skull", () -> SkullBlockAccessor.variantsandventures$createSkullBlock(SkullBlockType.VERDANT.get(), AbstractBlock.Settings.create().instrument(Instrument.SKELETON).strength(1.0F).pistonBehavior(PistonBehavior.DESTROY)));
+	public static Supplier<Block> VERDANT_WALL_SKULL = BLOCKS.register("verdant_wall_skull", () -> WallSkullBlockAccessor.variantsandventures$createWallSkullBlock(SkullBlockType.VERDANT.get(), AbstractBlock.Settings.create().instrument(Instrument.SKELETON).strength(1.0F).pistonBehavior(PistonBehavior.DESTROY).dropsLike(VERDANT_SKULL.get())));
 
 	private VariantsAndVenturesBlocks() {
 	}
