47d1354880dd944a9fd9550a9e9b28e8671462e1
Aleš Fabiánek <fabianek.ales@gmail.com>
2024-03-04 08:30:25 +0100

    wip

--- forge/src/main/java/com/faboslav/variantsandventures/common/platform/forge/ConfigDirectoryImpl.java
+++ forge/src/main/java/com/faboslav/variantsandventures/common/platform/forge/ConfigDirectoryImpl.java
@@ -6,9 +6,6 @@ import java.nio.file.Path;
 
 public final class ConfigDirectoryImpl
 {
-	/**
-	 * @see com.faboslav.variantsandventures.platform.ConfigDirectory#getConfigDirectory()
-	 */
 	public static Path getConfigDirectory() {
 		return FMLPaths.CONFIGDIR.get();
 	}
