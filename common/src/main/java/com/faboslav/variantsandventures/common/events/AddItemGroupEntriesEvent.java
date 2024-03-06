847bf90a71455fa5faadb4837800922fcfd0c003
Aleš Fabiánek <fabianek.ales@gmail.com>
2024-03-06 22:07:06 +0100

    wip

--- common/src/main/java/com/faboslav/variantsandventures/common/events/AddItemGroupEntriesEvent.java
+++ common/src/main/java/com/faboslav/variantsandventures/common/events/AddItemGroupEntriesEvent.java
@@ -3,8 +3,13 @@ package com.faboslav.variantsandventures.common.events;
 import com.faboslav.variantsandventures.common.events.base.EventHandler;
 import net.minecraft.item.ItemConvertible;
 import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemGroups;
 import net.minecraft.item.ItemStack;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.util.Util;
 
+import java.util.IdentityHashMap;
+import java.util.Map;
 import java.util.function.Consumer;
 
 /**
@@ -15,7 +20,7 @@ import java.util.function.Consumer;
  * @author ThatGravyBoat
  * <a href="https://github.com/Team-Resourceful/ResourcefulLib">https://github.com/Team-Resourceful/ResourcefulLib</a>
  */
-public record AddItemGroupEntriesEvent(ItemGroup itemGroup, boolean hasPermission, Consumer<ItemStack> adder) {
+public record AddItemGroupEntriesEvent(Type type, ItemGroup itemGroup, boolean hasPermission, Consumer<ItemStack> adder) {
 
     public static final EventHandler<AddItemGroupEntriesEvent> EVENT = new EventHandler<>();
 
@@ -26,4 +31,40 @@ public record AddItemGroupEntriesEvent(ItemGroup itemGroup, boolean hasPermissio
     public void add(ItemConvertible item) {
         adder.accept(new ItemStack(item));
     }
+
+	public enum Type {
+		BUILDING_BLOCKS,
+		COLORED_BLOCKS,
+		NATURAL,
+		FUNCTIONAL,
+		REDSTONE,
+		HOTBAR,
+		SEARCH,
+		TOOLS,
+		COMBAT,
+		FOOD_AND_DRINK,
+		INGREDIENTS,
+		SPAWN_EGGS,
+		OPERATOR,
+		INVENTORY,
+		CUSTOM;
+
+		public static Type toType(ItemGroup itemGroup) {
+			if (ItemGroups.BUILDING_BLOCKS.equals(itemGroup)) return BUILDING_BLOCKS;
+			else if (ItemGroups.COLORED_BLOCKS.equals(itemGroup)) return COLORED_BLOCKS;
+			else if (ItemGroups.NATURAL.equals(itemGroup)) return NATURAL;
+			else if (ItemGroups.FUNCTIONAL.equals(itemGroup)) return FUNCTIONAL;
+			else if (ItemGroups.REDSTONE.equals(itemGroup)) return REDSTONE;
+			else if (ItemGroups.HOTBAR.equals(itemGroup)) return HOTBAR;
+			else if (ItemGroups.SEARCH.equals(itemGroup)) return SEARCH;
+			else if (ItemGroups.TOOLS.equals(itemGroup)) return TOOLS;
+			else if (ItemGroups.COMBAT.equals(itemGroup)) return COMBAT;
+			else if (ItemGroups.FOOD_AND_DRINK.equals(itemGroup)) return FOOD_AND_DRINK;
+			else if (ItemGroups.INGREDIENTS.equals(itemGroup)) return INGREDIENTS;
+			else if (ItemGroups.SPAWN_EGGS.equals(itemGroup)) return SPAWN_EGGS;
+			else if (ItemGroups.OPERATOR.equals(itemGroup)) return OPERATOR;
+			else if (ItemGroups.INVENTORY.equals(itemGroup)) return INVENTORY;
+			return CUSTOM;
+		}
+	}
 }
