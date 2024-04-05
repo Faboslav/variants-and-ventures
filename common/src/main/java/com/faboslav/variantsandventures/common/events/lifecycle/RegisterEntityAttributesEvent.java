a7121b6e1bbe321a8b27c7248d59e65ce3e87d7f
Aleš Fabiánek <fabianek.ales@gmail.com>
2024-04-05 08:13:07 +0200

    Added missing credits

--- common/src/main/java/com/faboslav/variantsandventures/common/events/lifecycle/RegisterEntityAttributesEvent.java
+++ common/src/main/java/com/faboslav/variantsandventures/common/events/lifecycle/RegisterEntityAttributesEvent.java
@@ -7,6 +7,14 @@ import net.minecraft.entity.attribute.DefaultAttributeContainer;
 
 import java.util.function.BiConsumer;
 
+/**
+ * Event related is code based on The Bumblezone/Resourceful Lib mods with permissions from the authors
+ *
+ * @author TelepathicGrunt
+ * <a href="https://github.com/TelepathicGrunt/Bumblezone">https://github.com/TelepathicGrunt/Bumblezone</a>
+ * @author ThatGravyBoat
+ * <a href="https://github.com/Team-Resourceful/ResourcefulLib">https://github.com/Team-Resourceful/ResourcefulLib</a>
+ */
 public record RegisterEntityAttributesEvent(
 	BiConsumer<EntityType<? extends LivingEntity>, DefaultAttributeContainer.Builder> attributes)
 {
--- common/src/main/java/com/faboslav/variantsandventures/common/events/lifecycle/RegisterEntitySpawnRestrictionsEvent.java
+++ common/src/main/java/com/faboslav/variantsandventures/common/events/lifecycle/RegisterEntitySpawnRestrictionsEvent.java
@@ -6,6 +6,14 @@ import net.minecraft.entity.SpawnRestriction;
 import net.minecraft.entity.mob.MobEntity;
 import net.minecraft.world.Heightmap;
 
+/**
+ * Event related is code based on The Bumblezone/Resourceful Lib mods with permissions from the authors
+ *
+ * @author TelepathicGrunt
+ * <a href="https://github.com/TelepathicGrunt/Bumblezone">https://github.com/TelepathicGrunt/Bumblezone</a>
+ * @author ThatGravyBoat
+ * <a href="https://github.com/Team-Resourceful/ResourcefulLib">https://github.com/Team-Resourceful/ResourcefulLib</a>
+ */
 public record RegisterEntitySpawnRestrictionsEvent(Registrar registrar)
 {
 	public static final EventHandler<RegisterEntitySpawnRestrictionsEvent> EVENT = new EventHandler<>();
