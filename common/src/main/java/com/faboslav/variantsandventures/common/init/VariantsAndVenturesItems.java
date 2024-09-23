95abe46d6d895cc1e3b9f3e2cbdcdc8f1b34ba3a
Faboslav <fabianek.ales@gmail.com>
2024-09-23 14:47:24 +0200

    Fixed egg colors

--- common/src/main/java/com/faboslav/variantsandventures/common/init/VariantsAndVenturesItems.java
+++ common/src/main/java/com/faboslav/variantsandventures/common/init/VariantsAndVenturesItems.java
@@ -19,10 +19,10 @@ public final class VariantsAndVenturesItems
 {
 	public static final ResourcefulRegistry<Item> ITEMS = ResourcefulRegistries.create(Registries.ITEM, VariantsAndVentures.MOD_ID);
 
-	public final static RegistryEntry<Item> GELID_SPAWN_EGG = ITEMS.register("gelid_spawn_egg", () -> new DispenserAddedSpawnEgg(VariantsAndVenturesEntityTypes.GELID, 0x108E9C, 0xDEF7F7, new Item.Settings().maxCount(64)));
-	public final static RegistryEntry<Item> MURK_SPAWN_EGG = ITEMS.register("murk_spawn_egg", () -> new DispenserAddedSpawnEgg(VariantsAndVenturesEntityTypes.MURK, 12698049, 0xA276A9, new Item.Settings().maxCount(64)));
-	public final static RegistryEntry<Item> THICKET_SPAWN_EGG = ITEMS.register("thicket_spawn_egg", () -> new DispenserAddedSpawnEgg(VariantsAndVenturesEntityTypes.THICKET, 0x005B57, 0x2B4913, new Item.Settings().maxCount(64)));
-	public final static RegistryEntry<Item> VERDANT_SPAWN_EGG = ITEMS.register("verdant_spawn_egg", () -> new DispenserAddedSpawnEgg(VariantsAndVenturesEntityTypes.VERDANT, 0x8B8A75, 0x4A5D21, new Item.Settings().maxCount(64)));
+	public final static RegistryEntry<Item> GELID_SPAWN_EGG = ITEMS.register("gelid_spawn_egg", () -> new DispenserAddedSpawnEgg(VariantsAndVenturesEntityTypes.GELID, 0xFF108E9C, 0xFFDEF7F7, new Item.Settings().maxCount(64)));
+	public final static RegistryEntry<Item> MURK_SPAWN_EGG = ITEMS.register("murk_spawn_egg", () -> new DispenserAddedSpawnEgg(VariantsAndVenturesEntityTypes.MURK, 0xFFC1B1E1, 0xFFA276A9, new Item.Settings().maxCount(64)));
+	public final static RegistryEntry<Item> THICKET_SPAWN_EGG = ITEMS.register("thicket_spawn_egg", () -> new DispenserAddedSpawnEgg(VariantsAndVenturesEntityTypes.THICKET, 0xFF005B57, 0xFF2B4913, new Item.Settings().maxCount(64)));
+	public final static RegistryEntry<Item> VERDANT_SPAWN_EGG = ITEMS.register("verdant_spawn_egg", () -> new DispenserAddedSpawnEgg(VariantsAndVenturesEntityTypes.VERDANT, 0xFF8B8A75, 0xFF4A5D21, new Item.Settings().maxCount(64)));
 
 	private VariantsAndVenturesItems() {
 	}
