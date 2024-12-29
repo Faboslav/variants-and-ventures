8856b195ac74308a4a53e61cadbe5ede0c94630a
Faboslav <fabianek.ales@gmail.com>
2024-12-29 15:35:15 +0100

    Updated mixin

--- common/src/main/java/com/faboslav/variantsandventures/common/mixin/PersistentProjectileEntityMixin.java
+++ common/src/main/java/com/faboslav/variantsandventures/common/mixin/PersistentProjectileEntityMixin.java
@@ -1,25 +1,23 @@
 package com.faboslav.variantsandventures.common.mixin;
 
 import com.faboslav.variantsandventures.common.entity.mob.MurkEntity;
+import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import net.minecraft.world.entity.projectile.AbstractArrow;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(AbstractArrow.class)
 public abstract class PersistentProjectileEntityMixin
 {
-	@Inject(
+	@ModifyReturnValue(
 		method = "getWaterInertia",
-		at = @At("HEAD"),
-		cancellable = true
+		at = @At("RETURN")
 	)
-	public void variantsandventures$getWaterInertia(
-		CallbackInfoReturnable<Float> cir
-	) {
+	protected float variantsandventures$getWaterInertia(float originalWaterInertia) {
 		if (((ProjectileEntityAccessor) this).getOwner() instanceof MurkEntity) {
-			cir.setReturnValue(0.99f);
+			return 0.99F;
 		}
+
+		return originalWaterInertia;
 	}
 }
